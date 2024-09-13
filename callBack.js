8cb30f1c7a0c8bffe7d45cbdb3b24d84b1fd59a8
Shehin Thoombil <shehinthoombil@gmail.com>
2024-09-13 14:30:52 +0530

    problems

---
+++ callBack.js
@@ -0,0 +1,28 @@
+
+function A(B) {
+    console.log("fisrt")
+    B()
+}
+function B() {
+    console.log("second")
+
+}
+A()
+
+// function A() {
+//     console.log("1");
+// }
+
+// function B() {
+//     setTimeout(() => {
+//         console.log("2");
+//     }, 3000);
+// }
+
+// function C() {
+//     console.log("3");
+// }
+
+// A()
+// B()
+// C()
---
+++ reviewPractice.js
@@ -0,0 +1,13 @@
+//some method in js
+// const arr = [5, 8, 12, 7];
+// const res = arr.some((val) => val > 10);
+// console.log(res)
+
+//Array.from()
+// const str = "hello"
+// const arr = Array.from(str)
+// console.log(arr)
+
+//symbol()
+// const mySymbol = Symbol();
+// console.log(mySymbol);
